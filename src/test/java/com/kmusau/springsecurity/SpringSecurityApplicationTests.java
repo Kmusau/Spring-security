d8f4a5c5d19037c18fcb0dc052b84fd82c29a76e
Kmusau <kevomusa6565@gmail.com>
2023-06-18 21:56:49 +0300

    Initial commit

---
+++ src/test/java/com/kmusau/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmusau.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
