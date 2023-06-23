ceb058e1e1d19d9154afc148f274c810a0e916ea
Kmusau <kevomusa6565@gmail.com>
2023-06-23 12:31:50 +0300

    User entity and auth request

---
+++ src/main/java/com/kmusau/springsecurity/entity/UserEntity.java
@@ -0,0 +1,21 @@
+package com.kmusau.springsecurity.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    private Long userId;
+    private String username;
+    private String password;
+    private String roles;
+}
