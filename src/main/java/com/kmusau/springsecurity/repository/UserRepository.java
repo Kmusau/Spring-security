ba684cb35310034529b63b01642985e8eb4453f5
Kmusau <kevomusa6565@gmail.com>
2023-06-23 12:57:13 +0300

    Custom user details class and userservice with loadbyusername method

---
+++ src/main/java/com/kmusau/springsecurity/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.kmusau.springsecurity.repository;
+
+import com.kmusau.springsecurity.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+}
---
+++ src/main/java/com/kmusau/springsecurity/service/UserService.java
@@ -0,0 +1,43 @@
+package com.kmusau.springsecurity.service;
+
+import com.kmusau.springsecurity.entity.UserEntity;
+import com.kmusau.springsecurity.repository.UserRepository;
+import com.kmusau.springsecurity.util.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.support.OpenSessionInterceptor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserEntity> user = userRepo.findByUsername(username);
+        return user.map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Username "+username+" not found"));
+    }
+
+    //Create a user
+    public UserEntity addUser(UserEntity user) {
+        return userRepo.save(user);
+    }
+
+    //Fetch all users
+    public List<UserEntity> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    //Get a single user by Id
+    public UserEntity getUserById(long id) {
+        return userRepo.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
+}
